aee705064f0753ec2a7d50a6603f8a51ee1487e8
YuYoHan <zxzz8014@naver.com>
2025-05-19 22:44:50 +0900

    [Add] Kafka Producer 추가

---
+++ producers/src/main/java/com/example/producers/kafka/SimpleProducer.java
@@ -0,0 +1,37 @@
+package com.example.producers.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class SimpleProducer {
+    public static void main(String[] args) {
+        // 메시지 보낼 TOPIC 이름 설정
+        String topicName = "simple-topic";
+
+        // KafkaProducer 환경 설정
+        Properties props = new Properties();
+        // bootstrap.servers, Producer -> Serialization -> Broker -> Consumer -> Deserialization
+        // key.serializer.class, value.serializer.class
+        // null, "hello world"
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // KafkaProducer 객체 생성
+        KafkaProducer<Object, String> kafkaProducer = new KafkaProducer<>(props);
+
+        // ProducerRecord 객체 생성
+        ProducerRecord<Object, String> producerRecord = new ProducerRecord<>(topicName, "hello kafka");
+
+        // 메시지 전송
+        kafkaProducer.send(producerRecord);
+
+        // 메시지 flush 및 close
+        kafkaProducer.flush();
+        kafkaProducer.close();
+    }
+}
