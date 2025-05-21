f1012b6386a6922f36f774e90e22eafc6cb8e9b0
YuYoHan <zxzz8014@naver.com>
2025-05-21 15:27:34 +0900

    [Study] Producer : 피자 주문 메시지 생성

---
+++ producers/src/main/java/com/example/producers/kafka/PizzaProducer.java
@@ -0,0 +1,115 @@
+package com.example.producers.kafka;
+
+import net.datafaker.Faker;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+
+public class PizzaProducer {
+    public static final Logger LOGGER = LoggerFactory.getLogger(PizzaProducer.class.getName());
+
+    public static void sendPizzaMessage(KafkaProducer<String, String> kafkaProducer,
+                                        String topicName, int iterCount,
+                                        int interIntervalMillis,
+                                        int intervalMillis,
+                                        int intervalCount,
+                                        boolean sync) {
+        PizzaMessage pizzaMessage = new PizzaMessage();
+        int iterSeq = 0;
+        long send = 2025;
+        Random random = new Random(send);
+        Faker faker = new Faker(new Locale("ko"), random); // or Locale.ENGLISH
+
+        while (iterSeq != iterCount) {
+            HashMap<String, String> pMessage = pizzaMessage.produce_msg(faker, random, iterSeq);
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, pMessage.get("key"), pMessage.get("message"));
+            sendMessage(kafkaProducer, producerRecord, pMessage, sync);
+
+            if((intervalCount > 0) && (iterSeq % intervalCount == 0)) {
+                try {
+                    LOGGER.info("IntervalCount : " + intervalCount + " intervalMills : " + interIntervalMillis);
+                    Thread.sleep(intervalMillis);
+                } catch (InterruptedException e) {
+                    LOGGER.info(e.getMessage());
+                }
+            }
+        }
+    }
+
+    public static void sendMessage(KafkaProducer<String, String> kafkaProducer,
+                                   ProducerRecord<String, String> producerRecord,
+                                   HashMap<String, String> pMessage, boolean sync) {
+        if(!sync) {
+            // KafkaProducer message send
+            // new Callback을 람다식으로 변형
+            kafkaProducer.send(producerRecord, (metadata, exception) -> {
+                if(exception == null) {
+                    LOGGER.info("record metadata received \n" +
+                            "partition : " + metadata.partition() + "\n" +
+                            "offset : " + metadata.offset() + "\n" +
+                            "timestamp : " + metadata.timestamp());
+                } else {
+                    LOGGER.error("exception error from broker " + exception.getMessage());
+                }
+            });
+        } else {
+            try (kafkaProducer) {
+                // KafkaProducer message send
+                RecordMetadata recordMetadata = kafkaProducer.send(producerRecord).get();
+                LOGGER.info("record metadata received \n" +
+                        "partition : " + recordMetadata.partition() + "\n" +
+                        "offset : " + recordMetadata.offset() + "\n" +
+                        "timestamp : " + recordMetadata.timestamp());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        // 메시지 보낼 TOPIC 이름 설정
+        String topicName = "pizza-topic";
+
+        // KafkaProducer 환경 설정
+        Properties props = new Properties();
+        // bootstrap.servers, Producer -> Serialization -> Broker -> Consumer -> Deserialization
+        // key.serializer.class, value.serializer.class
+        // null, "hello world"
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // KafkaProducer 객체 생성
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+
+        for(int seq = 0; seq < 20; seq++) {
+            // ProducerRecord 객체 생성
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, String.valueOf(seq), "hello kafka " + seq);
+
+
+        }
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        sendPizzaMessage(kafkaProducer, topicName, -1, 10, 100, 100, true);
+
+        // 메시지 flush 및 close
+        kafkaProducer.flush();
+        kafkaProducer.close();
+    }
+}
