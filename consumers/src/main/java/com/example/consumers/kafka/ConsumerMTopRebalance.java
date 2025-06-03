8da10529bc2ae0bbf5fd5bbd1cb6e7fdeec7c863
YuYoHan <zxzz8014@naver.com>
2025-06-03 14:54:45 +0900

    [Study] consumer rebalancing

---
+++ consumers/src/main/java/com/example/consumers/kafka/ConsumerMTopRebalance.java
@@ -0,0 +1,69 @@
+package com.example.consumers.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class ConsumerMTopRebalance {
+    public static final Logger LOGGER = LoggerFactory.getLogger(ConsumerMTopRebalance.class.getName());
+    public static void main(String[] args) {
+//        String topicName = "pizza-topic";
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+//        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_01");
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-mtopic");
+        properties.setProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+        kafkaConsumer.subscribe(List.of("topic-p3-t1", "topic-p3-t2"));
+
+        // main thread
+        Thread mainThread = Thread.currentThread();
+
+
+        // main thread 끝나기전에 마지막으로 동작을 시킬 수 있도록 하는 메소드
+        // main thread 종료시 별도의 thread로 kafkaConsumer wakeup 실행
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                LOGGER.debug("main program starts to exit calling wakeup() method");
+                // kafkaConumser.poll() 중에 exception을 발생시키는 용도
+                kafkaConsumer.wakeup();
+
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        try {
+            while(true) {
+                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000));
+                for(ConsumerRecord record : consumerRecords) {
+                    LOGGER.debug("topice : {}, record key : {}, record value : {}, partition : {}, offset : {}",
+                            record.topic(), record.key(), record.value(), record.partition(), record.offset());
+                }
+            }
+        } catch (WakeupException e) {
+            LOGGER.error("wakeup exception has been called");
+        } finally {
+            LOGGER.debug("finally consumer is closing");
+            kafkaConsumer.close();
+        }
+
+    }
+}
